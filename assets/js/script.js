f8a0099394ef173ac18b30142ad82243d2f3b7bd
helena <helena.gilja@gmail.com>
2023-01-11 22:53:43 +0000

    questions object of arrays skeleton created, elements selected and stored in variables, initial pseudocode laid out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,40 @@
-let questions = [
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3 },
+// create questions object, containing all the questions, answer choices and correct answer
 
-  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+let questions = [
+  { question1: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+
+  { question2: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
 ];
 
 let currentQuestion = 0;
+
+// store elements in variables
+let start = document.querySelector("#start");
+let qTitle = document.querySelector("#question-title");
+let choices = document.querySelector("#choices");
+let submit = document.querySelector("#submit");
+
+// click event on start quiz to start the quiz
+start.addEventListener("click", function (event) {});
+
+// on 1st click event I need to display the 1st question (hook into #questions) - I need to access the question in the object and display it
+// every question contains buttons for each answer (hook into addEventListener #choices)
+//use eventDelegation for questions to add 1 event listener
+//otherwise need 4 for each answer (#choices)
+// when the answer is clicked, next question appears
+// if the answer is incorrect, 10secs subtracted from the timer
+// quiz ends if time === 0 || all the questions are answered
+// when the game ends, score is displayed
+// user can save their initials and their score
