7895264fca8a023b2cf25d54914b127558c65e83
helena <helena.gilja@gmail.com>
2023-01-11 22:30:43 +0000

    script.js created and connected to index.html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let questions = [
+  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3 },
+
+  { question: "", answers: ["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2 },
+];
+
+let currentQuestion = 0;
