0094a556816298ab209ad40fc64931b4a985526e
helena <helena.gilja@gmail.com>
2023-01-13 22:46:07 +0000

    logic.js renamed back insto script.js and connected as import/export did not work so questions were not accessible, wrapped a for loop for highscores into a function and also added sort for sorting them in descending order

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,12 +1,22 @@
-let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+function printHighscores() {
+  let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-// Append highscores to the new page
-let highscoresList = document.getElementById("highscores");
-for (let i = 0; i < highscores.length; i++) {
-  let newScore = document.createElement("li");
-  newScore.innerHTML = `${highscores[i].initials} - ${highscores[i].score}`;
-  highscoresList.appendChild(newScore);
+  // Sort highscores by score property in descending order
+  highscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  // Append highscores to the new page
+  let highscoresList = document.getElementById("highscores");
+  for (let i = 0; i < highscores.length; i++) {
+    let newScore = document.createElement("li");
+    newScore.innerHTML = `${highscores[i].initials} - ${highscores[i].score}`;
+    highscoresList.appendChild(newScore);
+  }
 }
+
+// Run the function when the page loads
+printHighscores();
 
 // Clear the localStorage
 function clearStorage() {
