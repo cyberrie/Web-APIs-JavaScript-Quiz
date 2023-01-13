c08ffcf2760eb2df42ce2855f0c73d0664faaab9
helena <helena.gilja@gmail.com>
2023-01-13 23:04:22 +0000

    clear local storage function was not working in highscores.js, as getElementById was not globally defined so highscores list was not accessible in highscoresList.innerHTML=

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,27 +1,29 @@
-function printHighscores() {
-  let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+let highscoresList = document.getElementById("highscores");
 
+// Function to print highscores
+function printHighscores() {
+  // Retrieve from local storage
+  let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
   // Sort highscores by score property in descending order
   highscores.sort(function (a, b) {
     return b.score - a.score;
   });
 
   // Append highscores to the new page
-  let highscoresList = document.getElementById("highscores");
+  highscoresList.innerHTML = "";
   for (let i = 0; i < highscores.length; i++) {
     let newScore = document.createElement("li");
     newScore.innerHTML = `${highscores[i].initials} - ${highscores[i].score}`;
     highscoresList.appendChild(newScore);
   }
 }
-
 // Run the function when the page loads
 printHighscores();
 
 // Clear the localStorage
 function clearStorage() {
   localStorage.clear();
-  highscoresList.innerHTML = "";
+  printHighscores();
 }
 // Add event listener to Clear Highscores buttons
 let clearBtn = document.querySelector("#clear");
